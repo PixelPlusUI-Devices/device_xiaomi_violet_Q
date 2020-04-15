9bb3727fb9580ed452d8542c27adf14a0249f257
udhaya68-ai <62876759+udhaya68-ai@users.noreply.github.com>
2020-04-16 01:49:29 +0530

    Update and rename aosp_violet.mk to carbon_violet.mk

--- carbon_violet.mk
+++ carbon_violet.mk
@@ -22,15 +22,11 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 IS_PHONE:= true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_violet
+PRODUCT_NAME := carbon_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-
-# PixelPlusUI Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.pixelplusui.maintainer=Saurav
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet" \
