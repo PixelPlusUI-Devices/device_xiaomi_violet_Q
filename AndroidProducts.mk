4acf889556994db211754186d1b5d5ef135dfa5e
udhaya68-ai <62876759+udhaya68-ai@users.noreply.github.com>
2020-04-16 01:46:38 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_violet.mk
+    $(LOCAL_DIR)/carbon_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_violet-userdebug \
-    aosp_violet-eng \
-    aosp_violet-user
+    carbon_violet-userdebug \
+    carbon_violet-eng \
+    carbon_violet-user
