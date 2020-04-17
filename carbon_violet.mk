e9677c79590c5704c815aa05a42cb5ab30c41877
udhaya68-ai <62876759+udhaya68-ai@users.noreply.github.com>
2020-04-17 06:14:36 +0530

    Update carbon_violet.mk

--- carbon_violet.mk
+++ carbon_violet.mk
@@ -18,7 +18,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 $(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
